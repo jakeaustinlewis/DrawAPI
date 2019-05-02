7dbcb71a18d5f138637e06f3ba99d8b3767338c7
Jake Lewis <jakeaustinlewis@gmail.com>
2019-05-02 15:37:17 -0500

    api end points for creating accounts and loggin in

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express');
 require('dotenv').config();
+
 const app = express();
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 mongoose.connect(`${process.env.DB_HOST}/StrokeWithFriends`, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.log('Could not connect to MongoDB'))
+  .catch(err => console.log('Could not connect to MongoDB'));
 
+// Controllers
+const user = require('./resource/user/user.controller.js');
 
-app.set('view engine', 'pug');
-app.set('views', './views'); // default
-
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static('public'));
+app.use(logger('dev'));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser());
 // app.use(helmet());
 // app.use(logger);
 
@@ -26,8 +29,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Controllers
-const user = require('./resource/user/user.controller.js');
+app.use('/user', user);
 
 // PORT
 const port = process.env.PORT || 3000;
--- resource/user/user.controller.js
+++ resource/user/user.controller.js
@@ -1,12 +1,11 @@
+
 const express = require('express');
-const router = new express.Router();
 const bcrypt = require('bcrypt');
 // const jwt = require('jsonwebtoken');
 // const permit = require('../../middleware/permit');
 const User = require('./user.model.js');
 
-
-
+const router = new express.Router();
 /**
  * Create a new user.
  * @param {String} req.body.firstName - User first name.
@@ -15,24 +14,53 @@ const User = require('./user.model.js');
  * @param {String} req.body.password - User password.
  */
 router.post('/', async (req, res) => {
-    if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.password) return res.status(500).send('Missing required fields.');
-  
-    const existing = await User.findOne({ email: req.body.email });
-    if (existing) return res.sendStatus(500);
-  
-    const hash = await bcrypt.hash(req.body.password, 10);
-    if (!hash) return res.status(500).send();
-  
-    let created;
-    try {
-      created = await User.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: hash,
-      });
-    } catch (e) {
-      return res.status(500).send('Server error creating new user');
+  if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.password) return res.status(500).send('Missing required fields.');
+
+  const existing = await User.findOne({ email: req.body.email });
+  if (existing) return res.sendStatus(500);
+
+  const hash = await bcrypt.hash(req.body.password, 10);
+  if (!hash) return res.status(500).send();
+
+  let created;
+  try {
+    created = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hash,
+    });
+  } catch (e) {
+    return res.status(500).send('Server error creating new user');
+  }
+  res.send(created);
+});
+
+/**
+ * Validate existing JWT or email/pass and issue a new JWT.
+ * @param {String} req.body.email - User email.
+ * @param {String} req.body.password - User password.
+ */
+router.post('/auth', async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(500).send();
+  }
+
+  const criteria = { email: req.body.email.toLowerCase() };
+
+  let user = await User.findOne(criteria);
+  if (!user || user.length > 1) {
+    return res.status(500).send();
+  }
+
+  user = user.toObject();
+
+  // Password check if passwords are hashed
+  bcrypt.compare(req.body.password, user.password, (err, valid) => {
+    if (err || !valid) {
+      return res.status(401).send();
     }
-    res.send(created);
+    res.send({ user });
   });
+});
+module.exports = router;
